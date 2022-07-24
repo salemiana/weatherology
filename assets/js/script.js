3869e21038d7374e5c8d45a970599aec419254e6
salem z <salem.zerezghi2@gmail.com>
2022-07-24 08:35:26 -0600

    working on data retrieving

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
-var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#city-search");
+ var userFormEl = document.querySelector("#user-form");
+ var cityInputEl = document.querySelector("#searchBtn");
+ 
+  var buttonHandler = function (event) {
+    event.preventDefault();
+ //get value from input
+ var city = cityInputEl.value.trim();
+//var  getCityForecast = function(city){ 
 
+ if (city-search) {
+    getCity(city-search);
+    city ="";
+ }
 
-const city = document.getElementById("city").value;
-
-const apiKey = "1243104a39fee9bac22480d3d97da492";
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&appid=${apiKey}`;
-// var icon ="https://api.openweathermap.org/img/w/" + data.list.weather[0].icon + ".png";
-//     console.log(icon);
-
-
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-//`https://api.openweathermap.org/data/2.5/forecast?q=Austin&appid=${apiKey}`
-
-const getCity= function(search)
-{
-    fetch(apiUrl).then(function(res) {
-     res.json().then(function(data) {
-        console.log(data);
+ var getCity = function(req) {
+    const apiKey = "1243104a39fee9bac22480d3d97da492";
+const apiUrl =
+  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+ }
+  //make a request
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
     })
-})
-}
+    .then(function (data) {
+      console.log(data);
+    });
+
+  document.getElementById("city-search").value = selectedButton;
+  getCity(selectedButton);
+};
+
+// add event listeners to form and button container
+userFormEl.addEventListener("btn", buttonHandler);
+
