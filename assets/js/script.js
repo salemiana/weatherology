e3819c418b0016399c2686c367b59e6cc50a989a
salem z <salem.zerezghi2@gmail.com>
2022-07-24 10:20:29 -0600

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,46 @@
- var userFormEl = document.querySelector("#user-form");
- var cityInputEl = document.querySelector("#searchBtn");
- 
-  var buttonHandler = function (event) {
-    event.preventDefault();
- //get value from input
- var city = userFormEl.value;
-//var  getCityForecast = function(city){ 
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#searchBtn");
+var cityEl = document.querySelector('#city-search');
+const apiKey = "1243104a39fee9bac22480d3d97da492";
+var buttonHandler = function (event) {
+  event.preventDefault();
+  //get value from input
+  var city = cityEl.value;
+  //var  getCityForecast = function(city){
+console.log(city);
+  if (city) { 
+   // getCity(city);
+   getCoordinates(city);
+  }
 
- if (city) {
-    getCity(city);
-    
- }
+   
+//   var getCity = function (city) {
+//     const apiKey = "1243104a39fee9bac22480d3d97da492";
+    // const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
- var getCity = function(city) {
-    const apiKey = "1243104a39fee9bac22480d3d97da492";
-const apiUrl =
-  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
- 
-  //make a request
-  fetch(apiUrl)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data, city);
-    });
-}
+//     //make a request
+//     fetch(apiUrl)
+//       .then(function (res) {
+//         return res.json();
+//       })
+//       .then(function (data) {
+//         console.log(data, city);
+//       });
+//   };
 
   var selectedButton = document.getElementById("city-search").value;
-  getCity(selectedButton);
+  //getCity(selectedButton);
 };
 
+var getCoordinates = function(city) {
+    var apiUrl1 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+     fetch(apiUrl1)
+       .then(function (res) {
+         return res.json();
+       })
+       .then(function (data) {
+         console.log(data, city);
+       });
+    }
 // add event listeners to form and button container
-userFormEl.addEventListener('click', buttonHandler);
-
+userFormEl.addEventListener("submit", buttonHandler);
