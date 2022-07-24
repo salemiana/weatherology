e1d32c8576fcf145167c7df9a647073314bbd4aa
salem z <salem.zerezghi2@gmail.com>
2022-07-24 09:25:43 -0600

    in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,33 @@
   var buttonHandler = function (event) {
     event.preventDefault();
  //get value from input
- var city = cityInputEl.value.trim();
+ var city = userFormEl.value;
 //var  getCityForecast = function(city){ 
 
- if (city-search) {
-    getCity(city-search);
-    city ="";
+ if (city) {
+    getCity(city);
+    
  }
 
- var getCity = function(req) {
+ var getCity = function(city) {
     const apiKey = "1243104a39fee9bac22480d3d97da492";
 const apiUrl =
   `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
- }
+ 
   //make a request
   fetch(apiUrl)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log(data, city);
     });
+}
 
-  document.getElementById("city-search").value = selectedButton;
+  var selectedButton = document.getElementById("city-search").value;
   getCity(selectedButton);
 };
 
 // add event listeners to form and button container
-userFormEl.addEventListener("btn", buttonHandler);
+userFormEl.addEventListener('click', buttonHandler);
 
